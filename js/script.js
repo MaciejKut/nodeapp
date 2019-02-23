f7c1b2c5900942f2828862029f073b79d48d102e
MaciejKut <maciej.kutzmann@gmail.com>
2019-02-23 19:21:57 +0100

    Add testing env app file script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 process.stdin.setEncoding('utf-8');
-
+//process.stdout.write('Wpisz komendę: /exit, env,');
 process.stdin.on('readable', function () {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        process.stdout.write('To wartość instruction' + instruction);
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stdout.write('Wrong instruction!\n');
+
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+
+            case 'env':
+                console.log(process.env);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+                break;
         }
+    } else {
+        process.stderr.write('Wrong instruction!\n');
     }
 });
